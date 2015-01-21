df88b2e04323b999ac74385c04dcbefaa8bea577
nalssee <kenjin@sdf.org>
2015-01-22 00:30:15 +0900

    CodeMirror cl mode fixed

--- CodeMirror/mode/commonlisp/commonlisp.js
+++ CodeMirror/mode/commonlisp/commonlisp.js
@@ -12,11 +12,13 @@
 "use strict";
 
 CodeMirror.defineMode("commonlisp", function (config) {
-  var specialForm = /^(block|let*|return-from|catch|load-time-value|setq|eval-when|locally|symbol-macrolet|flet|macrolet|tagbody|function|multiple-value-call|the|go|multiple-value-prog1|throw|if|progn|unwind-protect|labels|progv|let|quote)$/;
-  var assumeBody = /^with|^def|^do|^prog|case$|^cond$|bind$|when$|unless$/;
-  var numLiteral = /^(?:[+\-]?(?:\d+|\d*\.\d+)(?:[efd][+\-]?\d+)?|[+\-]?\d+(?:\/[+\-]?\d+)?|#b[+\-]?[01]+|#o[+\-]?[0-7]+|#x[+\-]?[\da-f]+)/;
-  var symbol = /[^\s'`,@()\[\]";]/;
-  var type;
+    var specialForm = /^(block|let*|return-from|catch|load-time-value|setq|eval-when|locally|symbol-macrolet|flet|macrolet|tagbody|function|multiple-value-call|the|go|multiple-value-prog1|throw|if|progn|unwind-protect|labels|progv|let|quote)$/;
+    // fixed 
+    var assumeBody = /^with|^def|^let|^do|^prog|case$|bind$|when$|unless$/;
+    // var assumeBody = /^with|^def|^do|^prog|case$|^cond$|bind$|when$|unless$/;
+    var numLiteral = /^(?:[+\-]?(?:\d+|\d*\.\d+)(?:[efd][+\-]?\d+)?|[+\-]?\d+(?:\/[+\-]?\d+)?|#b[+\-]?[01]+|#o[+\-]?[0-7]+|#x[+\-]?[\da-f]+)/;
+    var symbol = /[^\s'`,@()\[\]";]/;
+    var type;
 
   function readSym(stream) {
     var ch;
